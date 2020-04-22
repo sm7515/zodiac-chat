5366d6e9122d3478958505becec376155c413a03
Sijia Meng <sm7515@nyu.edu>
2020-04-22 12:35:01 -0400

    build

---
+++ frontend/build/precache-manifest.40d21880b1d376e25fcf5d212aee1263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5075db6d1f900373701a0a17f9eb2f07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e54530b5c53d19cf8e60",
+    "url": "/static/css/main.f3af42e0.chunk.css"
+  },
+  {
+    "revision": "c420d3fdd9c64d246dd4",
+    "url": "/static/js/2.893d139e.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.893d139e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54530b5c53d19cf8e60",
+    "url": "/static/js/main.ed3712f4.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
